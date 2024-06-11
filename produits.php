813550931d2f3117a60d21f7bdeb1c2aec4f3618
nathanhbt35 <nathanhubert18@gmail.com>
2024-06-11 16:50:25 +0200

    page produits
    produits

---
+++ produits.php
@@ -0,0 +1,115 @@
+<?php
+    include_once("header.php");
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<body>
+    <main>
+        <nav class="produits-nav">
+            <h1>
+                Nos produits
+            </h1>
+            <button>Tous</button>
+            <button>Sachet</button>
+            <button>Boîtes</button>
+            <button>Haribo</button>
+            <button>Carambar</button>
+        </nav>
+
+        <section class="produits-container">
+
+            <article class="produit">
+                <img src="media/images/produits/1.png" alt="Mini Banan's">
+                <h3>Nom Bonbons</h3>
+                <p>Description du bonbon</p>
+                <p id="produits-prix">Prix</h4>
+            </article>
+            <article class="produit">
+                <img src="media/images/produits/5.png" alt="Mini Banan's">
+                <h3>Nom Bonbons</h3>
+                <p>Description du bonbon</p>
+                <p id="produits-prix">Prix</h4>
+            </article>
+            <article class="produit">
+                <img src="media/images/produits/3.png" alt="Mini Banan's">
+                <h3>Nom Bonbons</h3>
+                <p>Description du bonbon</p>
+                <p id="produits-prix">Prix</h4>
+            </article>
+            <article class="produit">
+                <img src="media/images/produits/4.png" alt="Mini Banan's">
+                <h3>Nom Bonbons</h3>
+                <p>Description du bonbon</p>
+                <p id="produits-prix">Prix</h4>
+            </article>
+
+            <div class="produits-bar">
+
+            </div>
+
+            <article class="produit">
+                <img src="media/images/produits/1.png" alt="Mini Banan's">
+                <h3>Nom Bonbons</h3>
+                <p>Description du bonbon</p>
+                <p id="produits-prix">Prix</h4>
+            </article>
+            <article class="produit">
+                <img src="media/images/produits/5.png" alt="Mini Banan's">
+                <h3>Nom Bonbons</h3>
+                <p>Description du bonbon</p>
+                <p id="produits-prix">Prix</h4>
+            </article>
+            <article class="produit">
+                <img src="media/images/produits/3.png" alt="Mini Banan's">
+                <h3>Nom Bonbons</h3>
+                <p>Description du bonbon</p>
+                <p id="produits-prix">Prix</h4>
+            </article>
+            <article class="produit">
+                <img src="media/images/produits/4.png" alt="Mini Banan's">
+                <h3>Nom Bonbons</h3>
+                <p>Description du bonbon</p>
+                <p id="produits-prix">Prix</h4>
+            </article>
+
+            <div class="produits-bar">
+
+            </div>
+
+            <article class="produit">
+                <img src="media/images/produits/1.png" alt="Mini Banan's">
+                <h3>Nom Bonbons</h3>
+                <p>Description du bonbon</p>
+                <p id="produits-prix">Prix</h4>
+            </article>
+            <article class="produit">
+                <img src="media/images/produits/5.png" alt="Mini Banan's">
+                <h3>Nom Bonbons</h3>
+                <p>Description du bonbon</p>
+                <p id="produits-prix">Prix</h4>
+            </article>
+            <article class="produit">
+                <img src="media/images/produits/3.png" alt="Mini Banan's">
+                <h3>Nom Bonbons</h3>
+                <p>Description du bonbon</p>
+                <p id="produits-prix">Prix</h4>
+            </article>
+            <article class="produit">
+                <img src="media/images/produits/4.png" alt="Mini Banan's">
+                <h3>Nom Bonbons</h3>
+                <p>Description du bonbon</p>
+                <p id="produits-prix">Prix</h4>
+            </article>
+        </section>
+
+
+    </main>
+
+<?php
+    include_once("footer.php");
+?>
+
+</body>
+</html>
